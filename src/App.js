258150f88c02011a929d2b51af27a771554b9b3e
ShushanV <shushan.work@gmail.com>
2020-03-16 23:25:24 +0400

    monday changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import Profile from './components/Profile/Profile'
-import Dialogs from './components/Dialogs/Dialogs'
 import { Route } from 'react-router-dom'
 import AboutUs from './components/AboutUs/AboutUs';
 import Music from './components/Music/Music'
 import Settings from './components/Settings/Settings'
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import UsersContainer from './components/Users/UsersContainer'
 
 const App = (props)  => {
   return (
@@ -34,6 +34,7 @@ const App = (props)  => {
           <Route path='/about' render={ () => <AboutUs />} /> 
           <Route path='/music' render={ () => <Music /> } /> 
           <Route path='/settings' render={ () => <Settings />} /> 
+          <Route path='/users' render={ () => <UsersContainer />} />
         </div>
       </div>
   );
