11d47f780b65524c134392957eb8d6f9adad143a
Shushan Vardanyan <you@example.com>
2020-03-18 08:56:27 +0400

    class components changings

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,31 +1,45 @@
 import React from 'react'
 import styles from './Users.module.css'
-import *as axios from 'axios'
+import * as axios from 'axios'
 import userPhoto from '../../images/12.jpg'
 
 
-let Users= (props) => {
-    let getUsers = () =>{
-        if(props.users.length === 0){
+
+class Users extends React.Component {
+    // constructor (props){
+    //     super(props);
+
+    //     axios.get('https://social-network.samuraijs.com/api/1.0/users')
+    //     .then(response => {
+    //         this.props.setUsers(response.data.items)})
+    //     }
+        componentDidMount(){
             axios.get('https://social-network.samuraijs.com/api/1.0/users')
-                .then(response => {
-                    props.setUsers(response.data.items)
-                })
-    }
-    } 
-    
-    return <div>
-        <button onClick={getUsers} >getUsers</button>
+            .then(response => {
+                this.props.setUsers(response.data.items)})
+        }
+
+    // getUsers = () =>{
+    //     if(this.props.users.length === 0){
+    //         axios.get('https://social-network.samuraijs.com/api/1.0/users')
+    //             .then(response => {
+    //                 this.props.setUsers(response.data.items)
+    //             })
+    // }
+    //} 
+    render(){
+        return <div>
+        {/* <button onClick={this.getUsers} >getUsers</button> */}
         {
-            props.users.map(u => <div key={u.id}>
+            this.props.users.map(u => <div key={u.id}>
             <span>
                 <div >
                     <img src={ u.photos.small != null ? u.photos.small : userPhoto } alt='' className={styles.usersPhoto} />
                 </div>
                 <div>
                         { u.followed ? 
-                        <button onClick={() => {props.unfollow(u.id) }}>Unollow</button> :
-                        <button onClick={() => {props.follow(u.id) }}>follow</button> 
+                        <button onClick={() => {this.props.unfollow(u.id) }}>Unollow</button> :
+                        <button onClick={() => {this.props.follow(u.id) }}>follow</button> 
                         }
                 </div>
             </span>
@@ -50,6 +64,8 @@ let Users= (props) => {
             </div>)
         }
     </div>
-}
-
+    }
+    } 
+    
+    
 export default Users
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import Users from "./UsersC"
+import Users from "./Users"
 import { followAC, unfollowAC, setUsersAC }  from '../../redux/usersReducer'
 
 
